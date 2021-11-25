e0583d2edd6f22306be25c68c9ac601b33852b4d
Bernt Andreas Drange <bernt.andreas.drange@inderes.fi>
2021-11-25 16:13:25 +0200

    Add support for postgres 14, test 14, 13, 12, 11 and 10

--- plugins/lando-services/services/postgres/builder.js
+++ plugins/lando-services/services/postgres/builder.js
@@ -9,14 +9,15 @@ const _ = require('lodash');
 module.exports = {
   name: 'postgres',
   config: {
-    version: '10',
-    supported: ['13', '12', '11', '11.1', '11.0', '10', '10.6.0', '9.6'],
+    version: '14',
+    supported: ['14', '13', '12', '11', '11.1', '11.0', '10', '10.6.0', '9.6'],
     pinPairs: {
-      '13': 'bitnami/postgresql:13.2.0-debian-10-r83',
-      '12': 'bitnami/postgresql:12.2.0-debian-10-r57',
-      '11': 'bitnami/postgresql:11.7.0-debian-10-r34',
-      '10': 'bitnami/postgresql:10.12.0-debian-10-r36',
-      '9.6': 'bitnami/postgresql:9.6.17-debian-10-r37',
+      '14': 'bitnami/postgresql:14.1.0-debian-10-r9',
+      '13': 'bitnami/postgresql:13.5.0-debian-10-r9',
+      '12': 'bitnami/postgresql:12.9.0-debian-10-r9',
+      '11': 'bitnami/postgresql:11.14.0-debian-10-r9',
+      '10': 'bitnami/postgresql:10.19.0-debian-10-r9',
+      '9.6': 'bitnami/postgresql:9.6.24-debian-10-r9',
     },
     patchesSupported: true,
     confSrc: __dirname,
