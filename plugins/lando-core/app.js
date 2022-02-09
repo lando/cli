59e413f2a7d41041de5881013b9f01f4ae987205
Mark Jaquith <mark@jaquith.me>
2022-02-09 16:00:13 -0500

    Fix missing space in scanning for services message (#124)

--- plugins/lando-core/app.js
+++ plugins/lando-core/app.js
@@ -226,7 +226,7 @@ module.exports = (app, lando) => {
   // Scan urls
   app.events.on('post-start', 10, () => {
     // Message to let the user know it could take a bit
-    console.log('Scanning to determine which services are ready... Please standby...');
+    console.log('Scanning to determine which services are ready... Please stand by...');
     // Filter out any services where the scanner might be disabled
     return app.scanUrls(_.flatMap(getScannable(app), 'urls'), {max: 16}).then(urls => {
       // Get data about our scanned urls
