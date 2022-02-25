c3dc10236ad1f352742940d24d86bd023cad2e89
John Ouellet <john@thinktandem.io>
2022-02-25 17:02:09 -0500

    Docs (#130)
    * Update to new docs
    
    * Add in linter
    
    * Rebuild packages
    
    * Adjust package.json
    
    * Adjust package.json
    
    * Adjust files

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,101 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Lando',
+  description: 'Lando CLI Docs.',
+  base: '/cli/',
+  head: [
+    ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1'}],
+    ['link', {rel: 'icon', href: '/cli/favicon.ico', size: 'any'}],
+    ['link', {rel: 'icon', href: '/cli/favicon.svg', type: 'image/svg+xml'}],
+    ['link', {rel: 'preconnect', href: '//fonts.googleapis.com'}],
+    ['link', {rel: 'preconnect', href: '//fonts.gstatic.com', crossorigin: true}],
+    ['link', {rel: 'stylesheet', href: '//fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap'}],
+  ],
+  theme: '@lando/vuepress-theme-default-plus',
+  themeConfig: {
+    landoDocs: true,
+    logo: '/images/icon.svg',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    repo: 'lando/cli',
+    sidebarHeader: {
+      enabled: false,
+    },
+    sidebar: [
+      {
+        text: 'Usage',
+        collapsible: false,
+        children: [
+          {
+            text: 'CLI Usage',
+            link: '/index.md',
+          },
+        ],
+      },
+      {
+        text: 'Commands',
+        collapsible: false,
+        children: [
+          {
+            text: 'lando config',
+            link: '/config.md',
+          },
+          {
+            text: 'lando destroy',
+            link: '/destroy.md',
+          },
+          {
+            text: 'lando init',
+            link: '/init.md',
+          },
+          {
+            text: 'lando info',
+            link: '/info.md',
+          },
+          {
+            text: 'lando list',
+            link: '/list.md',
+          },
+          {
+            text: 'lando logs',
+            link: '/logs.md',
+          },
+          {
+            text: 'lando poweroff',
+            link: '/poweroff.md',
+          },
+          {
+            text: 'lando rebuild',
+            link: '/rebuild.md',
+          },
+          {
+            text: 'lando restart',
+            link: '/restart.md',
+          },
+          {
+            text: 'lando share',
+            link: '/share.md',
+          },
+          {
+            text: 'lando ssh',
+            link: '/ssh.md',
+          },
+          {
+            text: 'lando start',
+            link: '/start.md',
+          },
+          {
+            text: 'lando stop',
+            link: '/stop.md',
+          },
+          {
+            text: 'lando version',
+            link: '/version.md',
+          },
+        ],
+      },
+      '/support.md',
+      {text: 'Release Notes', link: 'https://github.com/lando/cli/releases'},
+    ],
+  },
+};
---
+++ docs/support.md
@@ -0,0 +1,34 @@
+---
+title: Help and Support
+description: Get help with and support for the Lando CLI.
+---
+
+# Help and Support
+
+## Slack
+
+If you have a question or would like some community support we recommend you [join the Lando community on Slack](https://launchpass.com/devwithlando).
+
+## GitHub
+
+If you'd like to report a bug or submit a feature request then please [use the issue queue](https://github.com/lando/cli/issues/new/choose) in this repo.
+
+## Contact
+
+If you need priority and dedicated support, expediated bug fixes or more features then please contact us below:
+
+<form class="netlify" name="contact" method="POST" netflify data-netlify="true">
+  <input type="hidden" name="form-name" value="contact" />
+  <p>
+    <label>Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Submit</button>
+  </p>
+</form>
