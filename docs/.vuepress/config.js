0b5240f308d3fa66ac8f5a1a20611a25cadbc2d8
Mike Pirog <mike@lando.dev>
2022-12-01 23:03:41 -0500

    Switch CLI to show version information (#166)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,9 +20,11 @@ export default defineUserConfig({
     docsDir: 'docs',
     docsBranch: 'main',
     repo: 'lando/cli',
-    sidebarHeader: false,
-    versionsPage: false,
-    contributorsPage: false,
+    sidebarHeader: {
+      enabled: true,
+      title: 'Lando CLI',
+      icon: '/images/icon.png',
+    },
     sidebar: [
       {
         text: 'Usage',
