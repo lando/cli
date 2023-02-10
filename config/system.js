c2b4a2ce8c470413a9dc6974917a6aaf9953bbf1
Mike Pirog <mike@lando.dev>
2023-02-10 13:05:31 -0500

    modify config to be more like what it should be

--- config/system.js
+++ config/system.js
@@ -27,33 +27,29 @@ module.exports = ({options}) => {
       appfiles: ['base', 'dist', 'recipe', 'upstream', '', 'local', 'user'],
     },
     plugin: {
-      showCore: true,
-      // these are "additional" directories to scan for plugins on top of the "core/internal" that are loaded no
-      // matter what
-      dirs: {
-        cli: {
-          type: 'core',
+      dirs: [
+        {
+          id: 'cli',
           dir: path.resolve(__dirname, '..'),
           depth: 0,
         },
-        userCore: {
-          type: 'user',
+        {
+          id: 'core-updates',
           dir: path.join(dataDir, 'plugins', '@lando', 'core-next'),
           depth: 0,
         },
-        userCorePlugins: {
-          type: 'user',
+        {
+          id: 'core-plugins-updates',
           dir: path.join(dataDir, 'plugins', '@lando', 'core-next', 'plugins'),
           depth: 2,
         },
-        user: {
-          type: 'user',
+        {
+          id: 'user',
           dir: path.join(dataDir, 'plugins'),
           depth: 2,
         },
-      },
+      ],
     },
-    registry: {},
     system: {
       arch,
       bin,
@@ -89,22 +85,22 @@ module.exports = ({options}) => {
     updates: {
       notify: true,
     },
-    dockerDesktop: {
-      required: '>=3.6.5 && <=5.0.0',
-      scripts: path.resolve(root, 'scripts', 'docker-desktop'),
-      server: {
-        socketPath: (platform === 'win32') ? '//./pipe/docker_engine' : '/var/run/docker.sock',
-        // host: '192.168.1.10',
-        // port: process.env.DOCKER_PORT || 2375,
-        // ca: fs.readFileSync('ca.pem'),
-        // cert: fs.readFileSync('cert.pem'),
-        // key: fs.readFileSync('key.pem'),
-        // version: 'v1.25',
-      },
-      supported: '>=3.6.5 && <=4.10.5',
-      // npmrc: '//npm.pkg.github.com/:_authToken=GH_ACCESS_TOKEN\n@namespace:registry=https://npm.pkg.github.com',
-    },
-    dockerEngine: {
+    // dockerDesktop: {
+    //   required: '>=3.6.5 && <=5.0.0',
+    //   scripts: path.resolve(root, 'scripts', 'docker-desktop'),
+    //   server: {
+    //     socketPath: (platform === 'win32') ? '//./pipe/docker_engine' : '/var/run/docker.sock',
+    //     // host: '192.168.1.10',
+    //     // port: process.env.DOCKER_PORT || 2375,
+    //     // ca: fs.readFileSync('ca.pem'),
+    //     // cert: fs.readFileSync('cert.pem'),
+    //     // key: fs.readFileSync('key.pem'),
+    //     // version: 'v1.25',
+    //   },
+    //   supported: '>=3.6.5 && <=4.10.5',
+    //   // npmrc: '//npm.pkg.github.com/:_authToken=GH_ACCESS_TOKEN\n@namespace:registry=https://npm.pkg.github.com',
+    // },
+    // dockerEngine: {
       // required: '>=3.6.5 && <=5.0.0',
       // scripts: path.resolve(root, 'scripts', 'docker-desktop'),
       // server: {
@@ -117,12 +113,12 @@ module.exports = ({options}) => {
       //   // version: 'v1.25',
       // },
       // supported: '>=3.6.5 && <=4.10.5',
-    },
-    dockerPluginInstaller: {
-      image: 'node:16-alpine',
-    },
+    // },
+    // dockerPluginInstaller: {
+    //   image: 'node:16-alpine',
+    // },
     // Allows you to pass env value to Docker, Docker Compose, etc.
     // @TODO: figure out how to implement this exactly
-    env: {},
+    // env: {},
   };
 };
