d2dc4b69338952693b1bf5161505d90ce075db46
Mike Pirog <mike@lando.dev>
2023-03-29 16:54:37 -0400

    dedash rts

--- config/product.js
+++ config/product.js
@@ -2,29 +2,20 @@
 
 const fs = require('fs');
 const path = require('path');
+const read = require('@lando/core-next/utils/read-file');
 
-// const Config = require('@lando/core-next/config');
-// const Templator = require('@lando/core-next/Templator');
+const Config = require('@lando/core-next/config');
+const Templator = require('@lando/core-next/templator');
 
 module.exports = options => {
   // start by figuring out the "core" location
-  // @NOTE: we prefer external core over the internal one for cases where core has been updated externally or
-  // if core is being developed
   const inCore = path.resolve(__dirname, '..', 'node_modules', '@lando', 'core-next');
   const exCore = path.join(options.dataDir, 'plugins', '@lando', 'core-next');
   options.coreDir = fs.existsSync(exCore) ? exCore : inCore;
   // and where we should dump the result of the config
   options.configCache = path.join(options.cacheDir, `${options.id}-config.json`);
-
   // now that options is sort of set, get some stuff we need from it
   const {configCache, configDir, configFile, dataDir, env, id, logger} = options;
-
-  // get core location dependent deps
-  // @NOTE: we can probably get rid of this for things like Config/Templator once they are in a stable state
-  // just like we invoke debug directly in bin/lando.js
-  const Config = require(`${options.coreDir}/lib/config`);
-  const Templator = require(`${options.coreDir}/lib/templator`);
-  const read = require(`${options.coreDir}/utils/read-file`);
 
   // configuration templates we need to create
   const templates = [
@@ -35,13 +26,11 @@ module.exports = options => {
     // a "user" config file that can be manually edited by the user
     [path.join(__dirname, 'user.yaml'), path.join(configDir, 'config.yaml'), {debug: logger.extend('templator')}],
   ];
-
   // loop through and generate our templates as needed
   templates.map(targs => new Templator(...targs)).map(template => template.generate(options));
 
   // start our config collection and load config sources in decreasing priority
   const config = new Config({cached: configCache, debug: logger.extend('minstrapper-config'), env, id, managed: 'global'});
-
   // if we have a CLI provided config source then thats first
   if (configFile) config.overrides('userfile', read(configFile), {encode: false});
   // then load in product envvars
