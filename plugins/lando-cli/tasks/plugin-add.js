74a875a4ec6c07ac40fa123942873cb33000a789
Mike Pirog <mike@lando.dev>
2023-05-25 14:24:49 -0400

    utilify v3 plugin fetching

--- plugins/lando-cli/tasks/plugin-add.js
+++ plugins/lando-cli/tasks/plugin-add.js
@@ -5,21 +5,17 @@ module.exports = lando => {
     command: 'pa <plugin> [plugins...]',
     run: async options => {
       const _ = require('lodash');
-      const fs = require('fs-extra');
-      const path = require('path');
-
-      const {Manager} = require('listr2');
       const Plugin = require('@lando/core-next/plugin');
+      const {Manager} = require('listr2');
+
+      // reset Plugin.debug to use the lando 3 debugger
+      Plugin.debug = require('../../../util/debug-shim')(lando.log);
 
       // @TODOS:
       // * get auth to work
       // * get path based installation to work? npmcli@arborist?
       // * debug ultimate location?
-      // * @lando/core fix
       // * also sent user-agten
-
-      // reset Plugin.debug to use the lando 3 debugger
-      Plugin.debug = require('../../../util/debug-shim')(lando.log);
 
       // merge plugins together
       const plugins = [options.plugin].concat(options.plugins);
@@ -41,25 +37,9 @@ module.exports = lando => {
               // attempt to compute the destination to install the plugin
               // @NOTE: is it possible for this to ever be undefined?
               const {dir} = _.find(lando.config.pluginDirs, {type: require('../util/get-plugin-type')(plugin)});
-              // add the plugin
-              task.plugin = await Plugin.fetch(plugin, {dest: dir});
 
-              // lando 3 plugin loading is at odds with lando 4 plugin installing so we need to move up a directory
-              // if the plugin has org scope, also clean up dangling @lando/lando dir
-              if (task.plugin.name.split('/').length === 2) {
-                const src = task.plugin.location;
-                const dest = path.resolve(task.plugin.location, '..', '..', task.plugin.name.split('/')[1]);
-                const orgDir = path.resolve(task.plugin.location, '..', '..', task.plugin.name.split('/')[0]);
-                // move and log
-                fs.moveSync(src, dest, {overwrite: true});
-                lando.log.debug('%s org scoped plugin detected, moved up a dir to %s', task.plugin.name, task.plugin.location);
-                // remove and log
-                fs.rmSync(path.resolve(src, '..'), {recursive: true});
-                lando.log.debug('%s removed dangling and presumably/hopefully empty org scope dir %s', task.plugin.name, orgDir);
-                // get the plugin info again to confirm we moved it to the correct place
-                // @NOTE: we use new Plugin() here instead of Plugin.info to ensure task.plugin remains consistent
-                task.plugin = new Plugin(dest);
-              }
+              // add the plugin
+              task.plugin = await require('../util/fetch-plugin')(plugin, {dest: dir}, Plugin);
 
               // update and and return
               task.title = `Installed ${task.plugin.name}@${task.plugin.version}`;
---
+++ plugins/lando-cli/util/fetch-plugin.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs-extra');
+const path = require('path');
+
+/*
+ * brief wrapper to accomodate lando 3 conventions when using lando 4 plugin fetching
+ */
+module.exports = async (spec, opts, Plugin = require('@lando/core-next/plugin')) => {
+  // attempt to fetch the plugin
+  const plugin = await Plugin.fetch(spec, opts);
+
+  // lando 3 plugin loading is at odds with lando 4 plugin installing so we need to move up a directory
+  // if the plugin has org scope, also clean up dangling @lando/lando dir
+  if (plugin.name.split('/').length === 2) {
+    const src = plugin.location;
+    const dest = path.resolve(plugin.location, '..', '..', plugin.name.split('/')[1]);
+    const orgDir = path.resolve(plugin.location, '..', '..', plugin.name.split('/')[0]);
+    // move and log
+    fs.moveSync(src, dest, {overwrite: true});
+    plugin.debug('org scoped plugin detected, moved up a dir to %s', dest);
+    // remove and log
+    fs.rmSync(path.resolve(src, '..'), {recursive: true});
+    plugin.debug('removed dangling and presumably/hopefully empty org scope dir %s', orgDir);
+    // get the plugin info again to confirm we moved it to the correct place
+    // @NOTE: we use new Plugin() here instead of Plugin.info to ensure plugin remains consistent
+    return new Plugin(dest);
+  }
+
+  // otherwise return the original
+  return plugin;
+};
