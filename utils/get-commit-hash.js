e230204623c6b8f9ef9cd32062d4f7b835607ba2
Mike Pirog <mike@lando.dev>
2023-12-04 10:45:46 -0500

    better experience and things when running from source part 2

--- utils/get-commit-hash.js
+++ utils/get-commit-hash.js
@@ -3,18 +3,29 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (dir, {short = false} = {}) => {
-  // get head
-  const head = path.join(dir, '.git', 'HEAD');
-  // if we dont have it then return false
-  if (!fs.existsSync(head)) return false;
+module.exports = (dir, {
+  refFile = path.join(dir, '.git', 'HEAD'),
+  short = false,
+} = {}) => {
+  // if reffile doesnt exist then return false
+  if (!fs.existsSync(refFile)) return false;
 
-  // read file contents
-  const ref = fs.readFileSync(head, {encoding: 'utf-8'});
-  console.log(ref);
+  const gitBase = path.join(dir, '.git');
 
+  // if the refFile has another ref in it then we need to reset the reffile
+  if (fs.readFileSync(refFile, {encoding: 'utf-8'}).startsWith('ref: ')) {
+    const contents = fs.readFileSync(refFile, {encoding: 'utf-8'}).trim();
+    const parts = contents.split('ref: ');
+    const ref = parts[1] ? parts[1] : 'HEAD';
+    refFile = path.join(gitBase, ref);
+  }
 
+  // get the commit
+  const commit = fs.readFileSync(refFile, {encoding: 'utf-8'}).trim();
 
+  // if we are "short" then return first seven
+  if (short && typeof commit === 'string') return commit.slice(0, 7);
 
-  console.log(dir);
+  // otherwise just return the whole thing
+  return commit;
 };
