ab5cab890e8e859f7833081815ef03c1b009f9ae
Mike Pirog <mike@lando.dev>
2023-12-05 20:43:57 -0500

    D&C6

--- test/art.spec.js
+++ test/art.spec.js
@@ -5,22 +5,20 @@
 
 'use strict';
 
-// const _ = require('lodash');
-// const art = require('../lib/art');
-// const chai = require('chai');
+const _ = require('lodash');
+const art = require('../lib/art');
+const chai = require('chai');
 chai.should();
 
 describe('art', () => {
-  it('should vibe');
+  it('should return an object', () => {
+    art.should.be.an('object');
+  });
 
-  // it('should return an object', () => {
-  //   art.should.be.an('object');
-  // });
-
-  // _.forEach(_.keys(art), key => {
-  //   it(`it should run ${key} without an error and return a string`, () => {
-  //     const result = art[key]();
-  //     result.should.be.a('string');
-  //   });
-  // });
+  _.forEach(_.keys(art), key => {
+    it(`it should run ${key} without an error and return a string`, () => {
+      const result = art[key]();
+      result.should.be.a('string');
+    });
+  });
 });
--- test/table.spec.js
+++ test/table.spec.js
@@ -6,57 +6,55 @@
 'use strict';
 
 const chai = require('chai');
-// const Table = require('../lib/table');
+const Table = require('../lib/table');
 chai.should();
 
 describe('table', () => {
-  it('should vibe');
+  describe('#Table', () => {
+    it('should return a Table instance with correct default options', () => {
+      const table = new Table();
+      table.should.be.instanceof(Table);
+      table.options.head.should.be.empty;
+      table.border.should.be.true;
+      table.sort.should.be.false;
+      table.joiner.should.equal('\n');
+      table.keyColor.should.equal('cyan');
+      table.options.chars.should.have.property('bottom', '─');
+    });
 
-  // describe('#Table', () => {
-  //   it('should return a Table instance with correct default options', () => {
-  //     const table = new Table();
-  //     table.should.be.instanceof(Table);
-  //     table.options.head.should.be.empty;
-  //     table.border.should.be.true;
-  //     table.sort.should.be.false;
-  //     table.joiner.should.equal('\n');
-  //     table.keyColor.should.equal('cyan');
-  //     table.options.chars.should.have.property('bottom', '─');
-  //   });
+    it('should return a Table instance with custom options', () => {
+      const table = new Table({}, {border: false, keyColor: 'green', sort: true, joiner: ', '});
+      table.should.be.instanceof(Table);
+      table.options.head.should.be.empty;
+      table.border.should.be.false;
+      table.sort.should.be.true;
+      table.joiner.should.equal(', ');
+      table.keyColor.should.equal('green');
+      table.options.chars.should.have.property('bottom', '');
+    });
+  });
 
-  //   it('should return a Table instance with custom options', () => {
-  //     const table = new Table({}, {border: false, keyColor: 'green', sort: true, joiner: ', '});
-  //     table.should.be.instanceof(Table);
-  //     table.options.head.should.be.empty;
-  //     table.border.should.be.false;
-  //     table.sort.should.be.true;
-  //     table.joiner.should.equal(', ');
-  //     table.keyColor.should.equal('green');
-  //     table.options.chars.should.have.property('bottom', '');
-  //   });
-  // });
+  describe('#add', () => {
+    it('should add an objects keys and values as rows', () => {
+      const table = new Table({
+        taylor: 'swift',
+        miley: 'cyrus',
+        katy: 'perry',
+        ariand: 'grande',
+      });
+      table.should.have.lengthOf(4);
+    });
 
-  // describe('#add', () => {
-  //   it('should add an objects keys and values as rows', () => {
-  //     const table = new Table({
-  //       taylor: 'swift',
-  //       miley: 'cyrus',
-  //       katy: 'perry',
-  //       ariand: 'grande',
-  //     });
-  //     table.should.have.lengthOf(4);
-  //   });
+    it('should join array values with POSIX newline by default', () => {
+      const table = new Table();
+      table.add({members: ['crosby', 'stills', 'nash', 'young']});
+      table[0][1].should.equal(['crosby', 'stills', 'nash', 'young'].join('\n'));
+    });
 
-  //   it('should join array values with POSIX newline by default', () => {
-  //     const table = new Table();
-  //     table.add({members: ['crosby', 'stills', 'nash', 'young']});
-  //     table[0][1].should.equal(['crosby', 'stills', 'nash', 'young'].join('\n'));
-  //   });
-
-  //   it('should join array values with alternate opts.arrayJoiner if specified', () => {
-  //     const table = new Table();
-  //     table.add({members: ['crosby', 'stills', 'nash', 'young']}, {joiner: '='});
-  //     table[0][1].should.equal('crosby=stills=nash=young');
-  //   });
-  // });
+    it('should join array values with alternate opts.arrayJoiner if specified', () => {
+      const table = new Table();
+      table.add({members: ['crosby', 'stills', 'nash', 'young']}, {joiner: '='});
+      table[0][1].should.equal('crosby=stills=nash=young');
+    });
+  });
 });
