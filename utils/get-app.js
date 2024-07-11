d4d2c950fd6324ac64e567650b3554cc94550937
Mike Pirog <mike@lando.dev>
2024-07-11 12:10:49 -0400

    added positional, usage and appConfig support to task parsing (#274)
    * cledit
    
    * added positional, usage and appConfig support to task parsing
    
    * added positional, usage and appConfig support to task parsing part 2
    
    * added positional, usage and appConfig support to task parsing part 3

--- utils/get-app.js
+++ utils/get-app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const uniq = require('lodash/uniq');
 
 /*
  * Helper to load landofile
@@ -21,6 +22,25 @@ module.exports = (files, userConfRoot) => {
   config.name = config.name.toString();
   // slugify project
   config.project = require('@lando/core/utils/slugify')(config.name);
+
+  // discover a default/primary service for legacy considerations
+  const services = Object.entries(config?.services ?? []).map(([service, config]) => ({name: service, ...config}));
+  const apis = uniq(services.map(service => parseInt(service.api)));
+
+  // if the api is only v4 then look for a primary service or the first one if none is set
+  if (apis.length === 1 && parseInt(apis[0]) === 4) {
+    const service = services.find(service => service.primary === true) ?? services[0] ?? {};
+    config.primary = service.name;
+
+  // otherwise this is a mixed environment so default appserver or first non v4 service if it exists
+  } else {
+    const service = services.find(service => service.name === 'appserver') ?? services.find(service => parseInt(service.api) !== 4) ?? {};
+    config.primary = service.name;
+  }
+
+  // if we still dont have a primary service it might be because its hidden in a recipe, if that is the case then
+  // we can assume its just the appserver?
+  if (!config.primary && config.recipe) config.primary = 'appserver';
 
   // merge and return
   return require('lodash/merge')({}, config, {
